bc13c24b5f436df8431bea4de31f75f00775b183
Sajid Abdullah <sajid264@gmail.com>
2023-12-20 23:17:27 +0600

    Create 2.6.sol

---
+++ 2.6.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <0.9.0;
+
+contract b2mappv2.6{
+    
+    address superadmin;
+    constructor () {
+        superadmin = msg.sender;
+        allAdmins.push(superadmin);
+        isAdmin[superadmin] = true;
+    }
+    //Debuging start
+    address public lastPatient;
+    uint totalTestCount;
+    string lastTest;
+    // Debug Finish
+    address [] public allDocs;
+    address [] public allAdmins;
+    mapping (address => bool) isAdmin;
+    mapping (address => bool) isDoc;
+    mapping (address => bool) isUser;
+    
+    mapping (address => string[]) userTests;
+    mapping (address => mapping (address => bool)) sharedTests;
+    
+    modifier patientCheck{
+        require(isUser[msg.sender] == true, "Please Register as Patient.");
+        _;
+    }
+    modifier docCheck{
+        require(isDoc[msg.sender] == true, "Please Register as a doctor.");
+        _;
+    }
+    modifier adminCheck{
+        require(isAdmin[msg.sender] == true, "This Feature is only for admins.");
+        _;
+    }
+    event newPatient(address indexed ke, uint kkhon);
+    event newDoc(address indexed ke, uint kkhon, address indexed ApprovedBy);
+    event newAdmin(address indexed ke, uint kkhon);
+    event newTest(address indexed ke, uint kkhon);
+    event wrongCall(address indexed ke, bytes ki);
+    event dataShared (address indexed patient, address indexed doc);
+    event dataViewedByDoc (address indexed doc, uint kokhon);
+    event dataSharingTurnedOff (address doc, address patient, uint kokhon);
+
+    function registerPatient() public {
+        require(isAdmin[msg.sender] == false, "Admins cant be Patients!");
+        require(isDoc[msg.sender] == false, "Docs Cant be Patients");
+        isUser[msg.sender] = true;
+        lastPatient = msg.sender;
+        emit newPatient(msg.sender, block.timestamp);
+    }
+    function addAdmin (address admin) public {
+        require(msg.sender == superadmin, "Only Super Admin can approve Admin Accounts");
+        require(isUser[admin] == false, "Patients cant be Admins!");
+        require(isDoc[admin] == false, "Docs Cant be Patients");
+        isAdmin[admin] = true;
+        allAdmins.push(admin);
+        emit newAdmin(admin, block.timestamp);
+    }
+    function registerDoc(address docid) adminCheck public {
+        require(isAdmin[docid] == false, "Admins cant be Docs!");
+        require(isUser[docid] == false, "Patients cant be Docs!");
+        isDoc[docid] = true;
+        allDocs.push(docid);
+        emit newDoc(docid, block.timestamp, msg.sender);
+    }
+    function addTest(string memory _val) patientCheck public {
+        userTests[msg.sender].push(_val);
+        lastTest = _val;
+        totalTestCount ++;
+        emit newTest(msg.sender, block.timestamp);
+    }
+
+    function seeResults(uint Tid) patientCheck public view returns (string memory) {
+        return userTests[msg.sender][Tid];
+    }
+
+    function seeAllResults() patientCheck public view returns (string[] memory) {
+        return userTests[msg.sender];
+    }
+
+    function toTalnumberOfResults() patientCheck public view returns (uint) {
+        return userTests[msg.sender].length;
+    }
+
+    function shareTest (address docid) patientCheck public {
+        sharedTests[msg.sender][docid] = true;
+        emit dataShared(msg.sender, docid);
+    }
+
+    function seePatientRecord (address patientId, uint testId) docCheck public returns (string memory) {
+        require(sharedTests[patientId][msg.sender] == true, "Patient has not Shared.");
+        emit dataViewedByDoc(msg.sender, block.timestamp);
+        return string(userTests[patientId][testId]);
+    }
+    function removeDoc (address docid) patientCheck public {
+        require(sharedTests[msg.sender][docid] == true, "Doc is not in your share list.");
+        sharedTests[msg.sender][docid] = false;
+        emit dataSharingTurnedOff(docid, msg.sender, block.timestamp);
+    }
+    function removePatient (address patientId) docCheck public {
+        require(sharedTests[patientId][msg.sender] == true, "Patient has not shared data with you.");
+        sharedTests[patientId][msg.sender] = false;
+        emit dataSharingTurnedOff(msg.sender, patientId, block.timestamp);
+    }
+
+    function resetUser (address id) public {
+        require(msg.sender == superadmin, "Only Super Admin can Remove Patient Accounts");
+        if (isAdmin[id] = true){
+            isAdmin[id] = false;
+        } else if (isDoc[id] = true) {
+            isDoc[id] = false;
+        } else {
+            isUser[id] = false;
+        }
+    }
+    
+    fallback() external {
+        emit wrongCall(msg.sender, msg.data);
+    }
+
+    receive() external payable { }
+}
